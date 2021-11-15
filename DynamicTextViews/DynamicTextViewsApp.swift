e59cae05c4e56b0cbf99171a591d7326f080119e
Md Jubel Hossain <69891127+JubelHossaain@users.noreply.github.com>
2021-11-15 22:37:47 +0600

    Initial Commit

---
+++ DynamicTextViews/DynamicTextViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTextViewsApp.swift
+//  DynamicTextViews
+//
+//  Created by Md Jubel Hossain on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTextViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
